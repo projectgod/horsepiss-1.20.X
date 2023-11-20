726e8875b52e4af5e2a75f6dfcb77fd9c858c867
lespucci <46281476+lespucci@users.noreply.github.com>
2023-11-20 17:57:20 +0100

    Initial commit

---
+++ src/main/java/acc/buv/horsepiss/HorsePissClient.java
@@ -0,0 +1,10 @@
+package acc.buv.horsepiss;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HorsePissClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/acc/buv/horsepiss/HorsePissDataGenerator.java
@@ -0,0 +1,11 @@
+package acc.buv.horsepiss;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HorsePissDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
